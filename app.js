d77a1164e81cec262787c528bbffc761821bfcc9
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-10-13 10:15:57 -0300

    Config

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var app = angular.module('app', []);
 
+app.controller('controller', function($scope){
+  angular.extend($scope, {
+    config: {
+      title: 'Exemplo com a diretiva!'
+    }
+  });
+});
+
 app.directive('tooltip', function () {
   return {
     restrict: 'E',
-    link: function (scope, element, attrs) {
+    link: function (scope, element, attrs, teste) {
+      var config = scope[attrs['ngModel']];
       element.tooltip({
-        content: attrs.title
+        content: config.title
       });
     }
   }
